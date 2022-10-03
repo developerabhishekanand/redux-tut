fffaa12f3f573e93a098c2a4973da3d822672426
abhishek <developer.abhishekmca@gmail.com>
2022-10-03 20:03:38 +0530

    Prepare Redux Structure

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "../components/Home"
+
+ export const HomeContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+export default Home;
